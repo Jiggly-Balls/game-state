0064f6fadb82e586aff17ba72df4eb96cb279bc9
Jiggly Balls <jigglyballs9000@gmail.com>
2025-03-29 08:44:21 +0530

    Made Sate to an abstract & added on_event, on_enter and on_update methods

--- src/game_state/state.py
+++ src/game_state/state.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
+from abc import ABC
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional
+    from typing import Any, Optional
 
     from pygame import Surface
+    from pygame.event import Event
 
     from .manager import StateManager
 
 
-class State:
+class State(ABC):
     """The State class which works as an individual screen.
 
     :attributes:
@@ -26,16 +28,30 @@ class State:
     window: Optional[Surface] = None
     manager: Optional[StateManager] = None
 
-    def __init_subclass__(cls, state_name: Optional[str] = None) -> None:
+    def __init_subclass__(cls, *, state_name: Optional[str] = None) -> None:
         cls.state_name = state_name or cls.__name__
 
-    def setup(self) -> None:
-        """This method is only called once before ``State.run``, i.e right after the class
-        has been instantiated inside the StateManager. This method will never be called
-        ever again when changing / resetting States.
+    def on_setup(self) -> None:
+        """This method is only called once while being loaded into the `StateManager`.
+        This is also called when reloading the State.
+        This method should not be called manually.
         """
         pass
 
-    def run(self) -> None:
+    def on_event(self, event: Event) -> None:
+        """To be called when a pygame event needs to be processed."""
+        pass
+
+    def on_enter(self, prevous_state: State) -> None:
+        """This method is called once when a state has been switched and is
+        entering the current state."""
+        pass
+
+    def on_update(self, *args: Any) -> None:
         """The main game loop method to be executed by the ``StateManager``."""
+        pass
+
+    def on_leave(self, next_state: State) -> None:
+        """This method is called once when the state has been switched and is exiting
+        the current one"""
         pass
