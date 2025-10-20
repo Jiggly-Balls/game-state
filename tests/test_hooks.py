d530708bf3a078e93709fa3c33356564c4463d9b
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-20 15:15:35 +0530

    Added test case for loading invalid state hooks

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,4 +1,7 @@
+import pytest
+
 from src.game_state import StateManager
+from src.game_state.errors import StateError
 
 
 def test_hooks() -> None:
@@ -7,12 +10,22 @@ def test_hooks() -> None:
     state_manager.connect_state_hook("tests.test_hooks_states.hook_1")
     state_manager.connect_state_hook("tests.test_hooks_states.hook_2")
 
-    state_1_name = "HookState1"
+    STATE_1_NAME = "HookState1"
+    TOTAL_VALID_LOADED_STATES: int = 2
 
-    state_manager.change_state(state_1_name)
+    state_manager.change_state(STATE_1_NAME)
 
     assert state_manager.current_state is not None, "Expected non-None value."
 
-    assert state_manager.current_state.state_name == state_1_name, (
-        f"Expected `{state_1_name}` as current state, instead got {state_manager.current_state}"
+    assert state_manager.current_state.state_name == STATE_1_NAME, (
+        f"Expected `{STATE_1_NAME}` as current state, instead got {state_manager.current_state}"
+    )
+
+    with pytest.raises(StateError):
+        state_manager.connect_state_hook("tests.test_hooks_states.hook_3")
+
+    loaded_states = len(state_manager.state_map)
+
+    assert loaded_states == TOTAL_VALID_LOADED_STATES, (
+        f"Expected {TOTAL_VALID_LOADED_STATES} loaded states, insted got {loaded_states}"
     )
