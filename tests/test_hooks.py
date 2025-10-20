ca8052fa909604c439d8a14e1ddc14981beefabd
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-20 15:06:19 +0530

    Fixed hook tests

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -4,13 +4,15 @@ from src.game_state import StateManager
 def test_hooks() -> None:
     state_manager = StateManager(...)  # pyright: ignore[reportArgumentType]
 
-    state_manager.connect_state_hook("tests.test_hook_states.hook_1")
-    state_manager.connect_state_hook("tests.test_hook_states.hook_2")
+    state_manager.connect_state_hook("tests.test_hooks_states.hook_1")
+    state_manager.connect_state_hook("tests.test_hooks_states.hook_2")
 
     state_1_name = "HookState1"
 
     state_manager.change_state(state_1_name)
 
-    assert state_manager.current_state == state_1_name, (
+    assert state_manager.current_state is not None, "Expected non-None value."
+
+    assert state_manager.current_state.state_name == state_1_name, (
         f"Expected `{state_1_name}` as current state, instead got {state_manager.current_state}"
     )
