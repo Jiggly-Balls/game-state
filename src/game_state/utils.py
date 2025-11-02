a548f0bbcb8fbce4f6037065588f6143ca92d88e
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-02 16:04:10 +0530

    Created StateArg

--- src/game_state/utils.py
+++ src/game_state/utils.py
@@ -9,6 +9,33 @@ if TYPE_CHECKING:
 __all__ = ("MISSING",)
 
 
+class StateArgs:
+    def __init__(self, *, state_name: str, **kwargs: Any) -> None:
+        self.state_name = state_name
+        self.__dict__.update(kwargs)
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            + ", ".join(
+                (
+                    f"{key}={value}"
+                    for key, value in zip(
+                        self.__dict__.keys(), self.__dict__.values()
+                    )
+                )
+            )
+            + ")"
+        )
+
+    def __eq__(self, value: object) -> bool:
+        return isinstance(value, StateArgs) and value.__dict__ == self.__dict__
+
+    def __bool__(self) -> bool:
+        # The StateArg having data other than `state_name` will be considered truthy.
+        return len(self.__dict__) > 1
+
+
 class _MissingSentinel:
     __slots__: Tuple[str, ...] = ()
 
